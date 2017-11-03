44544f6912a04bbdaf7911e733c1327156207fea
Villy Siu <villysiu@gmail.com>
2017-11-03 18:56:11 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,18 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    if artist
+      self.artist.name
+    else
+      nil
+    end
   end
 
   def artist_name=(name)
+    if !name.empty?
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+  #  artist.add_song(self)
+end
   end
 end
